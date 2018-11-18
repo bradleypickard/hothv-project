dea1a533062c2e413c3bea43f210779ba23bd91c
RitvikAgarwal99 <45128859+RitvikAgarwal99@users.noreply.github.com>
2018-11-17 20:10:01 -0800

    Update script-updated.js

--- script-updated.js
+++ script-updated.js
@@ -8,7 +8,6 @@ let messageInput;
 
 let submitButton;
 let question;
-let userPic;
 let userName;
 
 let continuebutton;
@@ -24,29 +23,23 @@ let messagesRef;
 let provider;
 var count = 0;
 var count2 = 0;
-var testing = 5;
 
-var questionArray = new Array("Welcome to your new best friend when it comes to food on the Hill!",
-                              "For Breakfast, cereal or donuts ",
-                              "How many calories are in the chichen piadina",
-                              "What type of cuisine did you eat growing up:" + "\t" 
-                              + " 1 – American, 2 – Mexican.Spanish," + 
-                              "3 – Korean/Asian, 4 – Indian, 5 – American inspired international dishes, 6 – other",
-                              "How do you describe your current diet? 1. healthy"+
-                              "2. Unhealthy, 3 the same thing over and over, 4 unclear",
-                              "what do assoicate with the word drink: 1. orange juice 2. soda",
-                              "How likely to eat ethnic food: 1. very unlikely 2. unlikely 3. neutral 4. likely 5. very likely",
-                              "Was your favorite food cooked at home(1) or store bought(2) or both(3)?",
-                              "How likely are you to eat fruit on a regular day, from 1 - 5 (5 being most likely)",
-                              "How likely are you to eat greek food when available, from 1 - 5",
-                              "How often do you check nutritional values, from 1 - 5, 1 being never and 5 being always",
-                              "When you think of soup, do you think of veggie soup(1) or creamy soup(2) more?",
+var questionArray = new Array("Welcome to your new best friend when it comes to food on the hill!",
+                              "For breakfast do you prefer cereal or donuts?",
+                              "How many calories are in the Chichen Piadina?",
+                              "What type of cuisine did you eat growing up? 1. American 2. Mexican/Spanish 3. Korean/Asian 4. Indian 5. American inspired international dishes 6. Others",
+                              "How would you describe your current diet? 1. Healthy 2. Unhealthy, 3. Repetitive, 4 unclear",
+                              "What would associate with the word drink? 1. Orange juice or 2. Soda",
+                              "How likely are you to eat ethnic food? 1. Very unlikely 2. Unlikely 3. Neutral 4. Likely 5. Very likely",
+                              "Was your favorite food, 1. Cooked at home 2. Store bought 3. Both?",
+                              "How likely are you to eat fruit on a regular day on a scale of 1 – 5? (5 being most likely)",
+                              "How likely are you to eat Greek food when available on a scale of 1 – 5?",
+                              "How often do you check nutritional values on a scale of 1 – 5? (1 being never and 5 being always)",
+                              "When you think of soup, do you think of, 1. Veggie soup or 2. Creamy soup?"
+
                               );
 
 var answerArray = new Array (questionArray.length);
-
-
-
 
 // Enables or disables the submit button depending on the values of the input
 // fields.
@@ -109,7 +102,6 @@ let init = () => {
   messageForm = document.getElementById('message-form');
   messageInput = document.getElementById('message');
   submitButton = document.getElementById('submit');
-  userPic = document.getElementById('user-pic');
   userName = document.getElementById('user-name');
   question = document.getElementById('question');
   finished = document.getElementById('finished');
@@ -163,17 +155,15 @@ let loadMessages = () => {
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 let onAuthStateChanged = (user) => {
   if (user) { // User is signed in!
-    // Get profile pic and user's name from the Firebase user object.
-    const profilePicUrl = user.photoURL;   
+    // Get profile pic and user's name from the Firebase user object. 
     const userNameText = user.displayName;        
 
     // Set the user's profile pic and name.
-    userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
+
     userName.textContent = userNameText;
 
     // Show user's profile and sign-out button.
     userName.removeAttribute('hidden');
-    userPic.removeAttribute('hidden');
     signOutButton.removeAttribute('hidden');
 
     // Hide sign-in button.
@@ -187,7 +177,6 @@ let onAuthStateChanged = (user) => {
   } else { // User is signed out!
     // Hide user's profile and sign-out button.
     userName.setAttribute('hidden', 'true');
-    userPic.setAttribute('hidden', 'true');
     signOutButton.setAttribute('hidden', 'true');
 
     // Show sign-in button.
@@ -245,7 +234,7 @@ function setQuestion()
 
 
 // Displays a Message in the UI.
-let displayMessage = (key, name, text, picUrl) => {
+let displayMessage = (key, name, text) => {
 //   Set page Question before message display
   setQuestion();
   var div = document.getElementById(key);
